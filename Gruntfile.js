935015f4eb04ce9d0d76222eec8006853a9f6ea3
Luis Diego Cordero H <lcordero@growthaccelerationpartners.com>
2015-12-17 15:43:48 -0600

    PILOT-1932: Receive the vendors files as configuration.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ var path = require('path');
 var async = require('async');
 var glob = require('glob');
 
-module.exports = function(config, grunt) {
+module.exports = function (config, grunt) {
 
     var self = this;
 
@@ -22,16 +22,16 @@ module.exports = function(config, grunt) {
      * @param  {String} srcPath directory containing tenant specific files
      * @return {Array} array of Strings
      */
-    this.getTenants = function(srcPath){
+    this.getTenants = function (srcPath) {
 
-        function filter(file){
+        function filter(file) {
             return fs.statSync(path.join(srcPath, file)).isDirectory();
         }
 
-        if(fs.existsSync(srcPath)){
+        if (fs.existsSync(srcPath)) {
             var results = fs.readdirSync(srcPath).filter(filter);
             return results;
-        }else{
+        } else {
             return [];
         }
     };
@@ -44,7 +44,7 @@ module.exports = function(config, grunt) {
      * @param  {Array} fileList list of files to add to
      * @return {Array} list of files
      */
-    function getFiles(dir,fileList){
+    function getFiles(dir, fileList) {
         if (!fileList) {
             console.log("Variable 'fileList' is undefined or NULL.");
             return;
@@ -73,11 +73,11 @@ module.exports = function(config, grunt) {
      * @param  {String} src path tho look for the files
      * @return {Array<String>}
      */
-    this.getModuleFiles = function(module, element, src){
+    this.getModuleFiles = function (module, element, src) {
         var options = {
             'cwd': src + module
         };
-        return glob.sync('js/' + element + '/*.js',options);
+        return glob.sync('js/' + element + '/*.js', options);
     };
 
     /**
@@ -87,11 +87,11 @@ module.exports = function(config, grunt) {
      * @param  {String} src path tho look for the files
      * @return {Array<String>}
      */
-    this.getModuleViews = function(module, src){
+    this.getModuleViews = function (module, src) {
         var options = {
             'cwd': src + module
         };
-        return glob.sync('tpl/*.html',options);
+        return glob.sync('tpl/*.html', options);
     };
 
     /**
@@ -102,7 +102,7 @@ module.exports = function(config, grunt) {
      * @param  {[type]} module [description]
      * @return {[type]}        [description]
      */
-    this.getDeniedFilesForModule = function(files,src,module){
+    this.getDeniedFilesForModule = function (files, src, module) {
         var deniedFiles = [];
         var fileName = null;
         for (var i in files) {
@@ -119,27 +119,27 @@ module.exports = function(config, grunt) {
      * @param  {[type]} module [description]
      * @return {[type]}        [description]
      */
-    this.getTenantFilesForModule = function(files,src,tenant,module){
+    this.getTenantFilesForModule = function (files, src, tenant, module) {
         var tenantFiles = [];
         var fileName = null;
         for (var i in files) {
-            tenantFiles.push( src + tenant  + '/' + module + '/' + files[i]);
+            tenantFiles.push(src + tenant + '/' + module + '/' + files[i]);
         }
         return tenantFiles;
     };
 
-    this.getConcatFiles = function(modules,elements,src,dest,tenantsPath) {
+    this.getConcatFiles = function (modules, elements, src, dest, tenantsPath) {
 
-        function getElementPatterns(path, elements, module, srcPath, tenant){
+        function getElementPatterns(path, elements, module, srcPath, tenant) {
             var src = [];
             var element = null;
             var tenantFiles = null;
             var deniedFiles = null;
             for (var i in elements) {
                 element = elements[i];
-                src.push(path  + element + '/*.js');
-                if(tenant) {
-                    tenantFiles = self.getModuleFiles(module, element,  config.src + config.tenants + '/' + tenant + '/');
+                src.push(path + element + '/*.js');
+                if (tenant) {
+                    tenantFiles = self.getModuleFiles(module, element, config.src + config.tenants + '/' + tenant + '/');
                     deniedFiles = self.getDeniedFilesForModule(tenantFiles, srcPath, module);
                     tenantFiles = self.getTenantFilesForModule(tenantFiles, config.src + config.tenants + '/', tenant, module);
                     src = src.concat(deniedFiles, tenantFiles);
@@ -150,81 +150,45 @@ module.exports = function(config, grunt) {
             return src;
         }
 
-        function getModulesFiles(destPath,srcPath, modules, elements,tenant){
-            var files = [
-                {
-                    dest: destPath + 'vendor.merged',
-                    src: [
-                        srcPath + 'vendor/js/jquery.js',
-                        srcPath + 'vendor/js/jquery-ui.js',
-                        srcPath + 'vendor/js/angular.js',
-                        srcPath + 'vendor/js/angular-cookies.js',
-                        srcPath + 'vendor/js/angular-resource.js',
-                        srcPath + 'vendor/js/angular-animate.js',
-                        srcPath + 'vendor/js/ngStorage.js',
-                        srcPath + 'vendor/js/angular-ui-router.js',
-                        srcPath + 'vendor/js/angular-ui-utils.js',
-                        srcPath + 'vendor/js/ui-bootstrap.js',
-                        srcPath + 'vendor/js/bootstrap.js',
-                        srcPath + 'vendor/js/ui-bootstrap-tpls.js',
-                        srcPath + 'vendor/js/lodash.js',
-                        srcPath + 'vendor/js/restangular.js',
-                        srcPath + 'vendor/js/angular-translate.js',
-                        srcPath + 'vendor/js/ng-grid.js',
-                        srcPath + 'vendor/js/ng-grid-reorderable.js',
-                        srcPath + 'vendor/js/moment.js',
-                        srcPath + 'vendor/js/easyXDM.js',
-                        srcPath + 'vendor/js/ng-grid-2.0.14.js',
-                        srcPath + 'vendor/js/ng-grid-reorderable.js',
-                        srcPath + 'vendor/js/select2.js',
-                        srcPath + 'vendor/js/ui-select.js',
-                        srcPath + 'vendor/js/ui-select2.js',
-                        srcPath + 'vendor/js/angular-sanitize.js',
-                        srcPath + 'vendor/js/ng-csv.js',
-                        srcPath + 'vendor/js/chosen.jquery.js',
-                        srcPath + 'vendor/js/toaster.js',
-                        srcPath + 'vendor/js/jquery.dataTables.js',
-                        srcPath + 'vendor/js/angular-datatables.js',
-                        srcPath + 'vendor/js/jquery.dataTables.js',
-                        srcPath + 'vendor/js/dataTables.bootstrap.js',
-                        srcPath + 'vendor/js/gm.datepickerMultiSelect.js',
-                        srcPath + 'vendor/js/ng-img-crop.js',
-                        srcPath + 'vendor/js/highcharts.js',
-                        srcPath + 'vendor/js/highcharts-ng.js',
-                        srcPath + 'vendor/js/jstz.js',
-                        srcPath + 'vendor/js/angular-file-upload.js',
-                        srcPath + 'vendor/js/source-map.js',
-                        srcPath + 'vendor/js/ua-parser.js',
-                        srcPath + 'vendor/js/ng-ua-parser.js'
-                    ]
+        function getModulesFiles(destPath, srcPath, modules, elements, tenant) {
+            var files = [];
+            if (config.tasks && config.tasks.concat) {
+                for (var taskIndex in config.tasks.concat) {
+                    var task = config.tasks.concat[taskIndex];
+                    if (task) {
+                        files.push({
+                            dest: destPath + task.file,
+                            src: task.src
+                        });
+                    }
                 }
-            ];
+            }
             var module = null;
             for (var i in modules) {
                 module = modules[i];
                 var moduleFiles = {
                     dest: tenant ? config.dest + config.tenants + '/' + tenant + '/merged/' + module + '.merged' : destPath + module + '.merged',
-                    src: getElementPatterns(srcPath + module + '/js/',elements, module,srcPath,tenant)
+                    src: getElementPatterns(srcPath + module + '/js/', elements, module, srcPath, tenant)
                 };
                 files.push(moduleFiles);
             }
             return files;
         }
 
-        var files = getModulesFiles(dest + 'merged/',src,modules,elements, tenantsPath);
+        var files = getModulesFiles(dest + 'merged/', src, modules, elements, tenantsPath);
 
         var tenants = self.tenants;
 
         var tenant = null;
         for (var i in tenants) {
             tenant = tenants[i];
-            files = files.concat(getModulesFiles(dest +'merged/',src,modules,elements,tenant));
+            files = files.concat(getModulesFiles(dest + 'merged/', src, modules, elements, tenant));
         }
 
         return files;
     };
 
-    this.getUglifyFiles = function(){
+    this.getUglifyFiles = function () {
         var files = [{
             expand: true,
             cwd: config.dest + 'merged',
@@ -252,9 +216,9 @@ module.exports = function(config, grunt) {
         return files;
     };
 
-    this.getRecessFiles = function(modules,srcPath,destPath) {
+    this.getRecessFiles = function (modules, srcPath, destPath) {
 
-        function getTenantFiles(tenant){
+        function getTenantFiles(tenant) {
             var tenantFiles = [];
             for (var i in modules) {
                 module = modules[i];
@@ -271,7 +235,7 @@ module.exports = function(config, grunt) {
 
         for (var i in modules) {
             module = modules[i];
-            files.push(srcPath  + module + '/css/*.css');
+            files.push(srcPath + module + '/css/*.css');
         }
 
         config[destPath + 'css/style.css'] = files;
@@ -286,7 +250,7 @@ module.exports = function(config, grunt) {
             tenant = tenants[i];
             config = {};
             var tFiles = getTenantFiles(tenant);
-            if(tFiles.length > 0 ){
+            if (tFiles.length > 0) {
                 config[destPath + config.tenants + '/' + tenant + '/css/style.css'] = tFiles;
                 configs.push(config);
             }
@@ -296,19 +260,19 @@ module.exports = function(config, grunt) {
 
     };
 
-    this.getCopyFiles = function(){
+    this.getCopyFiles = function () {
         var files = [
             {
                 expand: true,
                 cwd: config.src,
-                src: ['**/*.eot','**/*.svg','**/*.ttf','**/*.woff'],
+                src: ['**/*.eot', '**/*.svg', '**/*.ttf', '**/*.woff'],
                 dest: config.dest + 'fonts/',
                 flatten: true
             },
             {
                 expand: true,
                 cwd: config.src,
-                src: ['**/*.jpg','**/*.png','**/*.gif'],
+                src: ['**/*.jpg', '**/*.png', '**/*.gif'],
                 dest: config.dest + 'img/',
                 flatten: true
             },
@@ -323,7 +287,7 @@ module.exports = function(config, grunt) {
             {
                 expand: true,
                 cwd: config.src + '/vendor/css',
-                src: ['*.png','*.gif'],
+                src: ['*.png', '*.gif'],
                 dest: config.dest + 'css/',
                 flatten: true
             }
@@ -338,7 +302,7 @@ module.exports = function(config, grunt) {
                 {
                     expand: true,
                     cwd: config.src + config.tenants + '/' + tenant + '/',
-                    src: ['**/*.eot','**/*.svg','**/*.ttf','**/*.woff'],
+                    src: ['**/*.eot', '**/*.svg', '**/*.ttf', '**/*.woff'],
                     dest: config.dest + config.tenants + '/' + tenant + '/fonts/',
                     flatten: true
                 }
@@ -346,7 +310,7 @@ module.exports = function(config, grunt) {
             files.push({
                 expand: true,
                 cwd: config.src + config.tenants + '/' + tenant + '/',
-                src: ['**/*.jpg','**/*.png','**/*.gif'],
+                src: ['**/*.jpg', '**/*.png', '**/*.gif'],
                 dest: config.dest + config.tenants + '/' + tenant + '/img/',
                 flatten: true
             });
@@ -355,7 +319,7 @@ module.exports = function(config, grunt) {
         return files;
     };
 
-    this.getCleanGrooming = function(){
+    this.getCleanGrooming = function () {
         var paths = [config.dest + 'merged/*'];
 
         var tenants = self.tenants;
@@ -367,7 +331,7 @@ module.exports = function(config, grunt) {
         return paths;
     };
 
-    this.getJadeFiles = function(){
+    this.getJadeFiles = function () {
         var files = {
             "dist/index.html": [
                 "src/tpl/index.jade"
@@ -385,7 +349,7 @@ module.exports = function(config, grunt) {
         return files;
     };
 
-    this.getNgTemplateFiles = function(){
+    this.getNgTemplateFiles = function () {
 
         var files = [];
         var src = [];
@@ -399,7 +363,7 @@ module.exports = function(config, grunt) {
         files.push({
             cwd: config.src,
             src: src,
-            dest:config.dest + 'js/templates.js'
+            dest: config.dest + 'js/templates.js'
         });
 
         var tenants = self.tenants;
@@ -421,14 +385,14 @@ module.exports = function(config, grunt) {
             files.push({
                 cwd: config.src,
                 src: src,
-                dest:config.dest + config.tenants + '/' + tenant + '/js/templates.js'
+                dest: config.dest + config.tenants + '/' + tenant + '/js/templates.js'
             });
         }
 
         return files;
     };
 
-    this.getReplaceFiles = function(){
+    this.getReplaceFiles = function () {
         var files = [
             {
                 src: [config.dest + 'index.html'],
@@ -475,10 +439,10 @@ module.exports = function(config, grunt) {
         return files;
     };
 
-    this.getConfig = function(){
-        config.concatFiles = self.getConcatFiles(config.modules,config.elements,config.src,config.dest);
+    this.getConfig = function () {
+        config.concatFiles = self.getConcatFiles(config.modules, config.elements, config.src, config.dest);
         config.uglifyFiles = self.getUglifyFiles();
-        config.recessFiles = self.getRecessFiles(config.modules,config.src,config.dest);
+        config.recessFiles = self.getRecessFiles(config.modules, config.src, config.dest);
         config.copyFiles = self.getCopyFiles();
         config.cleanGroomingFiles = self.getCleanGrooming();
         config.jadeFiles = self.getJadeFiles();
